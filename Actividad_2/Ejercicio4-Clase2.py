c243c99ebf2c6b918df01e604857d00c0a315f20
DIEGO ANDRES PUMAREJO PADILLA <89914148+DiegoPumarejo0430@users.noreply.github.com>
2022-03-08 01:29:42 -0500

    Update Ejercicio4-Clase2.py

--- Actividad_2/Ejercicio4-Clase2.py
+++ Actividad_2/Ejercicio4-Clase2.py
@@ -1,69 +1,43 @@
-# **********************
-# manejo de listas 
-#********************
 
 
-"""
-
-manejo de listas 
-
-"""
-
-def listas():
- lista=[1,2]
- lista2 = list()
-
-listaconelementos = [30,2000000, "jaider quintero",True,"fin",["magister",24,"especializado"]] 
-#print(listaconelementos[3])
-print("*************ciclo for*****************")
-for i in range(len(listaconelementos)):
-    print(listaconelementos[i])
-
-print("***********ciclo while*************")
-
-j=0
-while j < len(listaconelementos):
-    print(listaconelementos[j])
-    j=j+1
-
-listaconelementos[1]=listaconelementos[1]+200
-print("****************** listas - posicion **********************")
-print(listaconelementos[5][2])
-print(listaconelementos[-1][2])
-print(listaconelementos[3:5])
-print(listaconelementos[1:6:2])
-print(listaconelementos[0:6:2])
-
-print(listaconelementos[1]+200)
-
-print("****************** add final **********************")
-listaconelementos.append("sede riohacha")
-print(listaconelementos)
-
-print("****************** add- posicion **********************")
-
-listaconelementos.insert(2,["pais","colombia"])
-
-print(listaconelementos)
-
-print("****************** del- posicion **********************")
-
-del listaconelementos[2]
-
-print(listaconelementos)
 
 
-print("****************** add- posicion **********************")
-
-listaconelementos.remove("jaider quintero")
-
-print(listaconelementos)
-
-def main ():
 
 
-    listas()
+def main():
 
-if __name__ == "__main__" :
+    #app de diccionarios
+#    persona = {
+ #   "nombre": "diego",
+ #   "apellido": "pumarejo",
+#  "edad" : 23
+#   }
 
+    persona = {
+
+    "datospersonales":{
+    "nombre" : "diego" ,
+    "apellido": "pumarejo",
+       "edad" : 23
+    },
+
+
+    "salarial" : {
+     "salario" : 2000000 ,
+      "subtransporte" : 50000 ,
+      "subalimentacon" : 60000
+
+
+    }
+
+    }
+
+   # print(persona["nombre"])
+   # print(f"{persona['nombre']} {persona['apellido']}    ")
+    print(f"Nombre: {persona['datos personales']['nombre']} Apellido: {persona['datos personales']['nombre']}  ")
+
+
+
+
+if __name__=="__main__":
     main()
