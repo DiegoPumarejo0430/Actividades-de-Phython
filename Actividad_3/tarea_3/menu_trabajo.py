38c469777305f01867f5f34b001caeb84780ce6e
DIEGO ANDRES PUMAREJO PADILLA <89914148+DiegoPumarejo0430@users.noreply.github.com>
2022-03-15 16:27:28 -0500

    condicionales

---
+++ Actividad_3/tarea_3/menu_trabajo.py
@@ -0,0 +1,21 @@
+
+
+
+def main():
+    opcion=int(input("digite una opcion: "))
+
+    if opcion==1 :
+        print("agregar empleados")
+    if opcion==2 :
+        print("eliminar empleado")
+    if opcion==3 :
+        print("mostrar empleado")
+    if opcion==4 :
+        print("salir")
+
+
+
+
+
+if __name__ == "__main__":
+ main()
