1e23d40f3c81734a4c3a16b64542da2f2d4b72ac
DIEGO ANDRES PUMAREJO PADILLA <89914148+DiegoPumarejo0430@users.noreply.github.com>
2022-03-15 16:40:35 -0500

    ajustando

--- Actividad_3/tarea_3/menu_trabajo.py
+++ Actividad_3/tarea_3/menu_trabajo.py
@@ -1,7 +1,9 @@
-
-
+import os
+def mostrar_lista(lista):
+  print(lista)  
 
 def main():
+    lista_empleados = [30,2000000, "Ing. Sistemas" ,"Yonatha Mdz", "Estudiante", True, ["Estudiar", "Mas Nada",23]]
     opcion=int(input("digite una opcion: "))
 
     if opcion==1 :
@@ -9,7 +11,10 @@ def main():
     if opcion==2 :
         print("eliminar empleado")
     if opcion==3 :
-        print("mostrar empleado")
+        os.system("cls")
+        print("MOSTRANDO LISTA DE EMPLEADOS...")
+        os.system("timeout /t 1 >null")
+        mostrar_lista(lista_empleados)
     if opcion==4 :
         print("salir")
 
