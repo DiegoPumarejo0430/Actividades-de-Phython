e39a241677fd7c0f3bd36a15af2b89c60d1d603b
DIEGO ANDRES PUMAREJO PADILLA <89914148+DiegoPumarejo0430@users.noreply.github.com>
2022-03-15 17:24:08 -0500

    mostrar y algunas correcciones

--- Actividad_3/tarea_3/menu_trabajo.py
+++ Actividad_3/tarea_3/menu_trabajo.py
@@ -1,25 +1,41 @@
 import os
 def mostrar_lista(lista):
-  print(lista)  
+    if len(lista) == 0 :
+        print ("**  lista vacia  **")
+    else:
+        for i in range(len(lista)):
+            print(lista[i])
 
 def main():
+    os.system("color 09")
     opcion = 0
-    lista_empleados = [30,2000000, "Ing. Sistemas" ,"Yonatha Mdz", "Estudiante", True, ["Estudiar", "Mas Nada",23]]
+    lista_empleados = []
     while opcion != 4 :
         os.system("cls")
-        print("\n")
-        print(f" 1. AGREGAR EMPLEADO A LA LISTA \n 2. ELIMINAR EMPLEADO DE LA LISTA \n 3.MOSTRAR LISTA DE EMPLEADOS \n 4. SALIR\n ")
+        print("\n ----------------------------------------------------------------------------")
+        print("                         LISTA DE EMPLEADOS                                    ")
+        print("\n ----------------------------------------------------------------------------")
+        print(f" 1. AGREGAR EMPLEADO A LA LISTA \n 2. ELIMINAR EMPLEADO DE LA LISTA \n 3. MOSTRAR LISTA DE EMPLEADOS \n 4. SALIR")
+        print("\n ----------------------------------------------------------------------------")
         opcion=int(input("digite una opcion: "))
-
+        
         if opcion==1 :
             print("Agregar Empleados")
+
+
         if opcion==2 :
             print("eliminar empleado")
+
+
+
+
         if opcion==3 :
             os.system("cls")
             print("MOSTRANDO LISTA DE EMPLEADOS...")
             mostrar_lista(lista_empleados)
-            os.system("timeout /t 3 >null")
+            os.system("timeout /t 5 >null")
+
+
         if opcion==4 :
             print("salir")
 
