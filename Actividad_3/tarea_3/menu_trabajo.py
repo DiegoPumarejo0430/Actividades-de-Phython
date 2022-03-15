47f9cccbe1616ce120db98f03dd3003f7eb4357a
DIEGO ANDRES PUMAREJO PADILLA <89914148+DiegoPumarejo0430@users.noreply.github.com>
2022-03-15 16:58:16 -0500

    mostrar

--- Actividad_3/tarea_3/menu_trabajo.py
+++ Actividad_3/tarea_3/menu_trabajo.py
@@ -3,20 +3,25 @@ def mostrar_lista(lista):
   print(lista)  
 
 def main():
+    opcion = 0
     lista_empleados = [30,2000000, "Ing. Sistemas" ,"Yonatha Mdz", "Estudiante", True, ["Estudiar", "Mas Nada",23]]
-    opcion=int(input("digite una opcion: "))
-
-    if opcion==1 :
-        print("agregar empleados")
-    if opcion==2 :
-        print("eliminar empleado")
-    if opcion==3 :
+    while opcion != 4 :
         os.system("cls")
-        print("MOSTRANDO LISTA DE EMPLEADOS...")
-        os.system("timeout /t 1 >null")
-        mostrar_lista(lista_empleados)
-    if opcion==4 :
-        print("salir")
+        print("\n")
+        print(f" 1. AGREGAR EMPLEADO A LA LISTA \n 2. ELIMINAR EMPLEADO DE LA LISTA \n 3.MOSTRAR LISTA DE EMPLEADOS \n 4. SALIR\n ")
+        opcion=int(input("digite una opcion: "))
+
+        if opcion==1 :
+            print("Agregar Empleados")
+        if opcion==2 :
+            print("eliminar empleado")
+        if opcion==3 :
+            os.system("cls")
+            print("MOSTRANDO LISTA DE EMPLEADOS...")
+            mostrar_lista(lista_empleados)
+            os.system("timeout /t 3 >null")
+        if opcion==4 :
+            print("salir")
 
 
 
