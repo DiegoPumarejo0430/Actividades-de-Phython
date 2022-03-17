552110fea6d8b1e2c63f73a835f09095269825ea
DIEGO ANDRES PUMAREJO PADILLA <89896231+gandresmendoza@users.noreply.github.com>
2022-03-16 22:52:51 -0500

    agregar empleado

--- Actividad_3/tarea_3/menu_trabajo.py
+++ Actividad_3/tarea_3/menu_trabajo.py
@@ -4,45 +4,74 @@ def mostrar_lista(lista):
         print ("**  lista vacia  **")
     else:
         for i in range(len(lista)):
-            print(lista[i])
+            print(f"{i+1} {lista[i]}")
+
+
+def Agregar_final(lista):
+    print("Agregar Empleados...")
+    nombre=input("Nombre(s) : ")
+    apellidos=input("Apellidos :")
+    telefono=int(input("Telefono: "))
+    edad=int(input("Edad: "))
+    direccion=input("Direccion:")
+
+    lista.append([nombre,apellidos,telefono,edad,direccion])
+
+    
+def Agregar_posicion(pos,lista):
+    print("...Agregar Empleados...")
+    nombre=input("Nombre(s): ")
+    apellidos=input("apellidos:")
+    telefono=int(input("telefono: "))
+    edad=int(input("Edad: "))
+    direccion=input("Direccion:")
+
+    lista.insert(pos, ([nombre,apellidos,telefono,edad,direccion]))
+
+
 
 def main():
-    opcion = 0
+    os.system("clear")
+    opcion = "0"
     lista_empleados = []
-    while opcion != 4 :
+    while opcion != "4":
         os.system("clear")
         print("\n ----------------------------------------------------------------------------")
         print("                         LISTA DE EMPLEADOS                                    ")
         print("\n ----------------------------------------------------------------------------")
         print(f" 1. AGREGAR EMPLEADO A LA LISTA \n 2. ELIMINAR EMPLEADO DE LA LISTA \n 3. MOSTRAR LISTA DE EMPLEADOS \n 4. SALIR")
         print("\n ----------------------------------------------------------------------------")
-        opcion=int(input("digite una opcion: "))
+        opcion=input("digite una opcion: ")
         
-        if opcion==1 :
-            print("Agregar Empleados")
+        if opcion=="1":
+            if len(lista_empleados) == 0:
+                Agregar_final(lista_empleados)
+            else: 
+                pos=int(input("posicion: "))
+                pos_h=pos-1
+                if pos>len(lista_empleados):
+                    Agregar_final(lista_empleados)
+                else :
+                    Agregar_posicion(pos_h,lista_empleados)
+                    # Agregar_final(lista_empleados)
+         
 
-            
-        if opcion==2 :
+        if opcion=="2" :
             
             print("eliminar empleado")
 
+            #yonatann
 
 
 
-        if opcion==3 :
+
+        if opcion=="3" :
             os.system("clear")
             print("MOSTRANDO LISTA DE EMPLEADOS...")
             mostrar_lista(lista_empleados)
-            time.sleep(3)
+        input()
 
 
-        if opcion==4 :
+        if opcion=="4" :
             print("cerrando...")
-            
-
-
-
-
-
-if __name__ == "__main__":
- main()
+        
